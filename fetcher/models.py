67e3a862bdfbae21c4abbb5432401300a42bafd4
Hastegan <hastegan@durbatuluk.net>
2016-11-04 14:56:53 +0100

    Add fetcher models

--- fetcher/models.py
+++ fetcher/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class DataSource(models.Model):
+
+    CATALOG = 'CAT'
+    TLE = 'TLE'
+    TYPE_CHOICES = (
+        (CATALOG, 'Catalog'),
+        (TLE, 'Two Line Elements')
+    )
+
+    type = models.CharField(
+        max_length = 3,
+        choices = TYPE_CHOICES,
+        default = CATALOG
+    )
+    name = models.CharField(
+        max_length=255
+    )
+    url = models.CharField(
+        max_length=255
+    )
+    comment = models.TextField()
+    date_added = models.DateTimeField()
+    last_time_checked = models.DateTimeField()
+    
