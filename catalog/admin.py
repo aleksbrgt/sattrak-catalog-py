b4c23678621a580bcb3db09f4df1fcbea5cee1cc
Hastegan <hastegan@durbatuluk.net>
2016-11-04 14:35:14 +0100

    Add models for catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import LaunchSite, OperationalStatus, OrbitalStatus, Source, CatalogEntry, TLE
 
-# Register your models here.
+admin.site.register(LaunchSite)
+admin.site.register(OperationalStatus)
+admin.site.register(OrbitalStatus)
+admin.site.register(Source)
+admin.site.register(CatalogEntry)
+admin.site.register(TLE)
