0fc13badafcced4d139adcb5da55dbbfde419472
hastegan <hastegan@durbatuluk.net>
2017-02-23 13:28:44 +0100

    unittest ans improvements on dates utils
    fixed the day number

--- api/tools/dates.py
+++ api/tools/dates.py
@@ -2,13 +2,30 @@
     Date related utility
 """
 
+import calendar
 from datetime import datetime, timedelta
 
-def get_days(date):
+from catalog.models import TLE
+
+def format_inline_time(inline_time):
+    return datetime.strptime(inline_time, '%Y%m%d%H%M%S')
+
+def date2fraction(date):
     """
         Return the fractionnal value of the current date/time in the year
     """
+
     start_year = datetime(date.year, 1, 1, 0, 0, 0, 0)
     diff = date - start_year
 
-    return diff.total_seconds() / timedelta(days=1).total_seconds() + 1
+    return diff.total_seconds() / timedelta(days=1).total_seconds()
+
+def fraction2date(fraction):
+    """
+        Return the month, day and time matching to the epoch
+    """
+
+    seconds = fraction * 86400
+
+    time = datetime.fromtimestamp(seconds)
+    return time
--- api/views.py
+++ api/views.py
@@ -73,10 +73,10 @@ class CatalogEntryViewSet(viewsets.ReadOnlyModelViewSet):
 
         time = datetime.utcnow()
         if given_time is not None:
-            time = datetime.strptime(given_time, '%Y%m%d%H%M%S')
+            time = dateutils.format_inline_time(given_time)
 
         last_digits_year = str(time.year)[-2:]
-        day_fraction = dateutils.get_days(time)
+        day_fraction = dateutils.date2fraction(time)
 
         tle = None
         try:
